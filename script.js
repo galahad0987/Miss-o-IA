14314459fd3fff1712ece2fdfa53733116932693
galahad0987 <guilherme.marangon.pereira@escola.pr.gov.br>
2026-09-08 15:58:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-principal");
+const caixaResultado = document.querySelector(".caixa-principal");
+const textoResultado = document.querySelector(".texto-resultado");
